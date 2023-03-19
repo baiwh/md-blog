3b3cc2508916acbf04bdcb3dade8c87cd5ec1607
✧*｡٩(ˊωˋ*)و✧*｡ <471974983@qq.com>
2023-03-19 17:42:03 +0800

    删除前后端分离的vue文件

--- pages/index/index.page.vue
+++ pages/index/index.page.vue
@@ -1,16 +1,3 @@
-<!-- <template>
-  <h1>Welcome</h1>
-  This page is:
-  <ul>
-    <li>Rendered to HTML.</li>
-    <li>Interactive. <Counter /></li>
-  </ul>
-</template>
-
-<script setup>
-import Counter from './Counter.vue'
-</script> -->
-
 <template>
   <div>
     home:{{ lala }}
