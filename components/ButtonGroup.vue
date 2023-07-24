b86a3739e993838e2cc74f807f39cac6cb6ec53e
✧*｡٩(ˊωˋ*)و✧*｡ <471974983@qq.com>
2023-07-25 01:22:53 +0800

    按钮组、图片组

---
+++ components/ButtonGroup.vue
@@ -0,0 +1,56 @@
+<template>
+  <div class="button-group">
+    <div v-for="(item, index) in btnData" v-bind:key="`btn-${index}}`">
+      <button :class="selectedBtn === item[label] ? 'button-item-selected' : 'button-item'"
+        @click="onClick(item)">{{ item[label] }}</button>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      selectedBtn: ''
+    }
+  },
+  props: ['btnData', 'label', 'onClickBtn'],
+  mounted() {
+  },
+  methods: {
+    onClick(item) {
+      this.selectedBtn = item[this.label]
+      this.onClickBtn(item)
+    },
+  }
+}
+</script>
+
+<style scoped lang="less">
+.button-group {
+  display: flex;
+  justify-content: flex-start;
+  flex-wrap: wrap;
+  flex-direction: row;
+
+  .button-item {
+    background-color: #fff;
+    color: #828282;
+    margin: 3px;
+    border: 1px solid transparent;
+  }
+
+  .button-item:hover {
+    color: #54b2ff;
+  }
+
+  .button-item-selected {
+    background-color: #fff;
+    margin: 3px;
+    color: #54b2ff;
+    border: 1px solid transparent;
+    border-bottom: 1px solid #54b2ff;
+  }
+}
+</style>
+
---
+++ components/ImgGroup.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="img-group">
+    <div v-for="(item, index) in imgList" v-bind:key="`hotel-${index}}`" class="hotelItem">
+      <img class="hotelImg" :src="item[srcKey]" />
+      <!-- @mouseover="mouseoverTest" @mouseleave="mouseleaveTest"  -->
+      <pre class="hotelDesc">{{ item[titleKey] }}</pre>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+    }
+  },
+  props: ['srcKey', 'titleKey', 'imgList'],
+  mounted() { },
+  methods: {
+    mouseoverTest() {
+      console.log('??')
+    },
+    mouseleaveTest() {
+      console.log('!!')
+    }
+  }
+}
+</script>
+
+<style scoped lang="less">
+.img-group{
+  display: flex;
+  justify-content: flex-start;
+  flex-wrap: wrap;
+  .hotelItem {
+  margin: 10px 10px 70px 10px;
+  position: relative;
+  
+
+  .hotelImg {
+    height: 300px;
+    width: 300px;
+  }
+
+  .hotelDesc {
+    position: absolute;
+    width: 300px;
+
+  }
+}
+}
+
+</style>
+
