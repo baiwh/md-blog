e11e724f853f98662548ec99f251a6a6efe8a38b
✧*｡٩(ˊωˋ*)و✧*｡ <471974983@qq.com>
2023-07-25 01:23:10 +0800

    汇丰酒店列表

---
+++ pages/hsbcHotel/index.page.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="visaCard">
+    <div class="hotelBox">
+      <button-group :btnData="cityList" :label="'cityName'" :onClickBtn="selectedCity" />
+      <img-group :imgList="filterHotel" :srcKey="'imageUrl'" :titleKey="'name'"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import cityList from './cityList.json';
+import hotel from './hotel.json';
+import ButtonGroup from '../../components/ButtonGroup.vue';
+import ImgGroup from '../../components/ImgGroup.vue';
+
+export default {
+  data() {
+    return {
+      cityList,
+      hotel,
+      filterHotel: [],
+    }
+  },
+  mounted() { },
+  components: {
+    ImgGroup,
+    ButtonGroup,
+  },
+  methods: {
+    selectedCity(city) {
+      this.filterHotel = this.hotel.filter(item => item.arriveCityCode == city.cityCode)
+    },
+  }
+}
+</script>
+
+<style scoped lang="less">
+.visaCard {
+  .hotelBox {}
+
+
+
+}
+</style>
